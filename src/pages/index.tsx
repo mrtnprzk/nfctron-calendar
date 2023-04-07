b854d5dd406152b6b4461e54b1216a022673c603
mrtnprzk <marti.parizek@gmail.com>
2023-04-07 20:37:18 +0200

    [ADD] react-big-calendar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,35 @@
+import moment from 'moment'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
 export default function Home() {
-    return <div className="text-red-500">Hello NFCtron!</div>
+    const localizer = momentLocalizer(moment)
+    const events = [
+        {
+            title: 'My Event',
+            start: new Date(),
+            end: new Date(),
+        },
+        {
+            title: 'My Event2',
+            start: new Date(),
+            end: new Date(),
+        },
+        {
+            title: 'My Event3',
+            start: new Date(),
+            end: new Date(),
+        },
+    ]
+
+    return (
+        <Calendar
+            localizer={localizer}
+            events={events}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: '100vh' }}
+        />
+    )
 }
